8b90a1daf26bbacccc3c7814b6766af679698f35
Andrew Barros <ajb5d@virginia.edu>
2023-12-05 19:49:15 -0500

    move h5 to scratch

--- tasks/create_datasets.sh
+++ tasks/create_datasets.sh
@@ -3,22 +3,21 @@
 sbatch create_dataset.slurm \
     --ecg_path /scratch/ajb5d/ecg/mimic-iv-ecg-diagnostic-electrocardiogram-matched-subset-1.0 \
     --file data/mimic/test_ecgs.csv \
-    --output data/mimic/test_ecgs_II.h5 \
+    --output /scratch/ajb5d/ecgdl/data/mimic/test_ecgs_II.h5 \
     --leads II \
     --fs 250
 
 sbatch create_dataset.slurm \
     --ecg_path /scratch/ajb5d/ecg/mimic-iv-ecg-diagnostic-electrocardiogram-matched-subset-1.0 \
     --file data/mimic/train_ecgs.csv \
-    --output data/mimic/train_ecgs_II.h5 \
+    --output /scratch/ajb5d/ecgdl/data/mimic/train_ecgs_II.h5 \
     --leads II \
     --fs 250
 
 sbatch create_dataset.slurm \
     --ecg_path /scratch/ajb5d/ecg/mimic-iv-ecg-diagnostic-electrocardiogram-matched-subset-1.0 \
     --file data/mimic/val_ecgs.csv \
-    --output data/mimic/val_ecgs_II.h5 \
+    --output /scratch/ajb5d/ecgdl/data/mimic/val_ecgs_II.h5 \
     --leads II \
     --fs 250
 
-    
